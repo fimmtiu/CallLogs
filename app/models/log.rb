3fb496edb90664e6ca15b51e78969dc8549dfb16
Dennis Taylor <dennis.taylor@clio.com>
2016-09-22 14:50:40 -0700

    Remove relevant files.

--- app/models/log.rb
+++
@@ -1,9 +0,0 @@
-class Log < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :customer
-
-  validates :user, presence: true
-  validates :customer, presence: true
-  validates :summary, length: {minimum: 1}
-  validates :duration, numericality: {greater_than: 0, only_integer: true}
-end
