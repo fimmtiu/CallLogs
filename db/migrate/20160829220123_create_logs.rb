6154f222a3a7cb5b901018d38bc9e8ba33db747e
Dennis Taylor <dennis.taylor@clio.com>
2016-09-22 14:57:40 -0700

    Revert "Remove relevant files."
    This reverts commit 3fb496edb90664e6ca15b51e78969dc8549dfb16.

---
+++ db/migrate/20160829220123_create_logs.rb
@@ -0,0 +1,23 @@
+class CreateLogs < ActiveRecord::Migration
+  def up
+    execute(<<-SQL
+      CREATE TABLE "logs" (
+        "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        "user_id" INTEGER,
+        "customer_id" INTEGER,
+        "summary" TEXT,
+        "duration" INTEGER,
+        "created_at" DATETIME,
+        "updated_at" DATETIME
+      )
+      SQL
+    )
+
+    execute('CREATE INDEX "index_logs_on_customer_id" ON "logs" ("customer_id")')
+    execute('CREATE INDEX "index_logs_on_user_id_and_created_at" ON "logs" ("user_id", "created_at")')
+  end
+
+  def down
+    execute('DROP TABLE "logs"')
+  end
+end
